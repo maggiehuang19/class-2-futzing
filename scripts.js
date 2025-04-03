e0316b0c7944a6a5d90d11f3209037b9cf093a90
maggiehuang19 <mh5919@nyu.edu>
2025-04-02 22:37:56 -0400

    test

--- scripts.js
+++ scripts.js
@@ -1,24 +1,68 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-    const img = document.getElementById("image-hover");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const img = document.getElementById("image-hover");
 
-    img.addEventListener("mouseover", function () {
-        img.src = "https://i.pinimg.com/736x/8a/e9/ff/8ae9ff0795b2563efc7ce3eff3c7bbb7.jpg";
-    });
+//     img.addEventListener("mouseover", function () {
+//         img.src = "https://i.pinimg.com/736x/8a/e9/ff/8ae9ff0795b2563efc7ce3eff3c7bbb7.jpg";
+//     });
 
-    img.addEventListener("mouseout", function () {
-        img.src = "https://i.imgur.com/IoV4q2j.jpeg";
-    });
-});
+//     img.addEventListener("mouseout", function () {
+//         img.src = "https://i.imgur.com/IoV4q2j.jpeg";
+//     });
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const img = document.getElementById("hover-image");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const img = document.getElementById("hover-image");
 
-    img.addEventListener("mouseover", function () {
-        img.src = "https://i.pinimg.com/736x/0b/6f/b9/0b6fb94ba35304648ca63ef086e58e61.jpg";
-    });
+//     img.addEventListener("mouseover", function () {
+//         img.src = "https://i.pinimg.com/736x/0b/6f/b9/0b6fb94ba35304648ca63ef086e58e61.jpg";
+//     });
 
-    img.addEventListener("mouseout", function () {
-        img.src = "https://i.imgur.com/s6ATtmv.jpeg";
-    });
-});
+//     img.addEventListener("mouseout", function () {
+//         img.src = "https://i.imgur.com/s6ATtmv.jpeg";
+//     });
+// });
+
+
+// strings
+// const firstName = "Maggie"
+// const lastName = "Huang"
+
+// const fullName = firstName + " " + lastName
+
+// console.log('fullName', fullName)
+
+// // use template literal notation
+// const fullNameAsTemplateLiteral = '${firstName} ${lastName} is '
+
+// console.log('fulllNameAsTemplateLiteral', fullNameAsTemplateLiteral)
+
+// // numbers
+// const age = 22
+// const ageInTenYears = age + 10
+
+// console.log('ageInTenYears', ageInTenYears)
+// console.log('ageAsString', age.toString())
+// console.log('parse Integer from string 44.5', parstInt(44.5))
+
+// // booleans
+// const maggieIsCool = true
+
+// console.log('the opposite of is maggie cool', !maggieIsCool)
+
+// if (chrisIsCool) {
+//     console.log('maggie is cool')
+// } else {
+//     console.log('maggie is not cool')
+// }
+
+// // arrays
+// const favoriteAlbums = ["Swimming", "Circles", "Blond"]
+
+// console.log('get the 2nd item in the array -->', favoriteAlbums[1])
+
+// favoriteAlbums.forEach(album) => {
+//     console.log('album in forEach loop', album)
+// }
+// // array of objects
+
